93185894bdf3f0bbee6030ebcc2110dcdf5e5d65
sak-gif <muhamad.mahmud.sak@gmail.com>
2025-08-22 20:21:47 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,2 @@
-from flask import Flask
 from app import app
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
