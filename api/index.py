e215d1cad254f49e6588ff93d01be927a754bdc1
sak-gif <muhamad.mahmud.sak@gmail.com>
2025-08-22 20:34:18 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,2 +1,8 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from app import app
 
+# handler pour Vercel
+def handler(event, context):
+    return app(event, context)
